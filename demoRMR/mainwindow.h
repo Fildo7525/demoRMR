9f1e996b5078e8fff44b34337185d9b1a5bc4bde
Filip Lobpreis <f.lobpreis@gmail.com>
2024-02-13 17:59:09 +0100

    demo: odometry

--- demoRMR/mainwindow.h
+++ demoRMR/mainwindow.h
@@ -25,59 +25,62 @@ namespace Ui {
 class MainWindow;
 }
 
-///toto je trieda s oknom.. ktora sa spusti ked sa spusti aplikacia.. su tu vsetky gombiky a spustania...
+/// toto je trieda s oknom.. ktora sa spusti ked sa spusti aplikacia.. su tu vsetky gombiky a spustania...
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
-
+	Q_OBJECT
 public:
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
 
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+	int processThisLidar(LaserMeasurement laserData);
 
-    int processThisLidar(LaserMeasurement laserData);
-
-    int processThisRobot(TKobukiData robotdata);
-
-
+	int processThisRobot(TKobukiData robotdata);
 
 private slots:
-    void on_pushButton_9_clicked();
+	void on_pushButton_9_clicked();
 
-    void on_pushButton_2_clicked();
+	void on_pushButton_2_clicked();
 
-    void on_pushButton_3_clicked();
+	void on_pushButton_3_clicked();
 
-    void on_pushButton_6_clicked();
+	void on_pushButton_6_clicked();
 
-    void on_pushButton_5_clicked();
+	void on_pushButton_5_clicked();
 
-    void on_pushButton_4_clicked();
+	void on_pushButton_4_clicked();
 
-    void on_pushButton_clicked();
-    void getNewFrame();
+	void on_pushButton_clicked();
+	void getNewFrame();
 
 private:
 
-    //--skuste tu nic nevymazat... pridavajte co chcete, ale pri odoberani by sa mohol stat nejaky drobny problem, co bude vyhadzovat chyby
-    Ui::MainWindow *ui;
-     void paintEvent(QPaintEvent *event);// Q_DECL_OVERRIDE;
-     int updateLaserPicture;
-     LaserMeasurement copyOfLaserData;
-     std::string ipaddress;
-     Robot robot;
-     TKobukiData robotdata;
-     int datacounter;
-     QTimer *timer;
+	//--skuste tu nic nevymazat... pridavajte co chcete, ale pri odoberani by sa mohol stat nejaky drobny problem, co bude vyhadzovat chyby
+	Ui::MainWindow *ui;
+	void paintEvent(QPaintEvent *event);// Q_DECL_OVERRIDE;
+	int updateLaserPicture;
+	LaserMeasurement copyOfLaserData;
+	std::string ipaddress;
+	Robot robot;
+	TKobukiData robotdata;
+	int datacounter;
+	QTimer *timer;
 
+	int lastLeftEncoder;
+	int lastRightEncoder;
+	double m_fi;
+	double m_x;
+	double m_y;
 
+	QTimer m_timer;
 
-     double forwardspeed;//mm/s
-     double rotationspeed;//omega/s
+	double forwardspeed;//mm/s
+	double rotationspeed;//omega/s
 public slots:
-     void setUiValues(double robotX,double robotY,double robotFi);
+	void setUiValues(double robotX,double robotY,double robotFi);
+	void timeout();
 signals:
-     void uiValuesChanged(double newrobotX,double newrobotY,double newrobotFi); ///toto nema telo
+	void uiValuesChanged(double newrobotX,double newrobotY,double newrobotFi); ///toto nema telo
 
 
 };
