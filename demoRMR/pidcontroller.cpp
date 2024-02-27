3b3c50850a81107fbf2632f19a64185136dd6ea8
Filip Lobpreis <f.lobpreis@gmail.com>
2024-02-27 22:18:09 +0100

    pid: create untiwindup if wanted
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- demoRMR/pidcontroller.cpp
+++ demoRMR/pidcontroller.cpp
@@ -7,6 +7,8 @@ PIDController::PIDController(double p, double i, double d, double target)
 	, m_target(target)
 	, m_integral(0)
 	, m_prev_error(0)
+	, m_clampedOutput(0)
+	, m_virginOutput(0)
 {
 }
 
@@ -16,11 +18,29 @@ double PIDController::compute(double current)
 	return computeFromError(error);
 }
 
-double PIDController::computeFromError(double error)
+double PIDController::computeFromError(double error, bool limit)
 {
 	m_integral += error;
-	double derivative = error - m_prev_error;
-	m_prev_error = error;
+	m_integral += m_clampedOutput - m_virginOutput;
 
-	return m_kp * error + m_ki * m_integral + m_kd * derivative;
+	double derivative = error - m_prev_error;
+
+	m_virginOutput = m_kp * error + m_ki * m_integral + m_kd * derivative;
+
+	if (limit) {
+		if (m_virginOutput < 0) {
+			m_clampedOutput = std::clamp(m_virginOutput, -250., -15.);
+		}
+		else {
+			m_clampedOutput = std::clamp(m_virginOutput, 15., 250.);
+		}
+	}
+
+	m_prev_error = m_clampedOutput;
+
+	if (limit) {
+		return m_clampedOutput;
+	}
+
+	return m_virginOutput;
 }
--- demoRMR/pidcontroller.h
+++ demoRMR/pidcontroller.h
@@ -8,6 +8,13 @@
 class PIDController : public QObject
 {
 	Q_OBJECT
+public:
+	PIDController(double p, double i, double d, double target = 0);
+	double compute(double current);
+	double computeFromError(double error, bool limit = false);
+	void setTarget(double newTarget) { m_target = newTarget; }
+	double target() const { return m_target; }
+
 private:
 	double m_kp;		 // Proportional gain
 	double m_ki;		 // Integral gain
@@ -15,13 +22,8 @@ private:
 	double m_target;	 // Target position
 	double m_integral;	 // Integral term
 	double m_prev_error; // Previous error
-
-public:
-	PIDController(double p, double i, double d, double target = 0);
-	double compute(double current);
-	double computeFromError(double error);
-	void setTarget(double newTarget) { m_target = newTarget; }
-	double target() const { return m_target; }
+	double m_virginOutput;
+	double m_clampedOutput;
 };
 
 #endif // PIDCONTROLLER_H
