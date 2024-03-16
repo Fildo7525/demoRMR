2ab601315b84c096c60d9f7518694c46c9ac6ec9
Filip Lobpreis <f.lobpreis@gmail.com>
2024-03-16 18:19:11 +0100

    planner: an object that planns the trajecotry
    The trajectory is planned using the flood algorithm. The map is taken from an external file.
    The file is the same that is used for the 3rd assignment. Thus, when the surrounding
    changes the only thing we need to do is reinitialize the scanning list with midpoints.
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- demoRMR/floodPlanner.cpp
+++ demoRMR/floodPlanner.cpp
@@ -1,6 +1,156 @@
 #include "floodPlanner.h"
 
-void FloodPlanner::loadMap(const std::string &filename)
-{
+#include <QFile>
+#include <QDebug>
+#include <iostream>
 
+FloodPlanner::FloodPlanner(const QString &filename)
+	: QObject(nullptr)
+	, m_map(new Map())
+{
+	loadMap(filename);
+}
+
+void FloodPlanner::loadMap(const QString &filename)
+{
+	m_map->clear();
+
+	fillMap(filename);
+	std::cout << "Loaded from file:\n" << *m_map << std::endl;
+	expandObstacles();
+	std::cout << "Expanded with obstacles:\n" << *m_map << std::endl;
+}
+
+QVector<QPointF> FloodPlanner::planPath(QPoint start, QPoint end)
+{
+	setStart(start);
+	setEnd(end);
+
+	Map localMap = *m_map;
+	QVector<QPointF> path;
+
+	markTiles(localMap, end, start);
+
+	std::cout << "Planned:\n" << localMap << std::endl;
+
+	return {};
+}
+
+void FloodPlanner::on_requestPath_plan(const QPoint &start, const QPoint &end)
+{
+	qDebug() << __FUNCTION__ << " " << start << " " << end;
+	auto path = planPath(start, end);
+	qDebug() << "Path: " << path;
+}
+
+void FloodPlanner::fillMap(const QString &filename)
+{
+	QFile file(filename);
+	if (!file.open(QIODevice::ReadOnly)) {
+		throw std::runtime_error("Failed to open file");
+	}
+
+	while (!file.atEnd()) {
+		QString line = file.readLine();
+		QVector<int> row;
+		for (auto c : line) {
+			if (c == '#') {
+				row.push_back(-1);
+			}
+			else {
+				row.push_back(0);
+			}
+		}
+		m_map->push_back(row);
+	}
+}
+
+void FloodPlanner::expandObstacles()
+{
+	for (int y = 0; y < m_map->size(); ++y) {
+		for (int x = 0; x < m_map->at(y).size(); ++x) {
+			if (m_map->at(y).at(x) == -1) {
+				if (y > 0 && m_map->at(y - 1).at(x) == 0) {
+					(*m_map)[y - 1][x] = -2;
+				}
+				if (y < m_map->size() - 1 && m_map->at(y + 1).at(x) == 0) {
+					(*m_map)[y + 1][x] = -2;
+				}
+				if (x > 0 && m_map->at(y).at(x - 1) == 0) {
+					(*m_map)[y][x - 1] = -2;
+				}
+				if (x < m_map->at(y).size() - 1 && m_map->at(y).at(x + 1) == 0) {
+					(*m_map)[y][x + 1] = -2;
+				}
+			}
+		}
+	}
+}
+
+FloodPlanner& FloodPlanner::setStart(QPoint start)
+{
+	m_start = start;
+	return *this;
+}
+
+FloodPlanner& FloodPlanner::setEnd(QPoint end)
+{
+	m_end = end;
+	return *this;
+}
+
+bool FloodPlanner::isTileValid(const Map &map, QPoint point)
+{
+	// Check for out of bounds.
+	if (point.x() < 0 || point.x() >= map[0].size()) {
+		return false;
+	}
+	if (point.y() < 0 || point.y() >= map.size()) {
+		return false;
+	}
+
+	// Check for obstacles.
+	if (map[point.y()][point.x()] < 0) {
+		return false;
+	}
+
+	return true;
+}
+
+void FloodPlanner::markTiles(Map &map, QPoint curr, QPoint end, int distance)
+{
+	if (map[curr.y()][curr.x()] == 0 || distance < map[curr.y()][curr.x()]) {
+		map[curr.y()][curr.x()] = distance;
+	}
+
+	if (curr == end) {
+		return;
+	}
+
+	for (size_t y = curr.y()-1; y < curr.y()+1; y++) {
+		for (size_t x = curr.x()-1; x < curr.x()+1; x++) {
+			if (isTileValid(map, QPoint(x, y)) && map[y][x] == 0) {
+				markTiles(map, QPoint(x, y), end, distance + 1);
+			}
+		}
+	}
+}
+
+std::ostream& operator<<(std::ostream &os, const FloodPlanner::Map &map)
+{
+	for (const auto &row : map) {
+		for (const auto &tile : row) {
+			if (tile == 0) {
+				os << ' ';
+			}
+			else if (tile > 0) {
+				os << tile;
+			}
+			else {
+				os << '#';
+			}
+		}
+		os << std::endl;
+	}
+	return os;
 }
--- demoRMR/floodPlanner.h
+++ demoRMR/floodPlanner.h
@@ -1,15 +1,42 @@
 #pragma once
 
+#include <optional>
 #include <memory>
-#include <vector>
+
+#include <QObject>
+#include <QPoint>
+#include <QString>
+#include <QVector>
 
 class FloodPlanner
+	: public QObject
 {
-	using Map = std::vector<std::vector<int>>;
+	using Map = QVector<QVector<int>>;
+	friend std::ostream &operator<<(std::ostream &os, const Map &map);
+
 public:
-	void loadMap(const std::string &filename);
+	explicit FloodPlanner(const QString &filename);
+	QVector<QPointF> planPath(QPoint start, QPoint end);
+
+// slots:
+	void on_requestPath_plan(const QPoint &start, const QPoint &end);
 
 private:
-	std::unique_ptr<Map> m_map;
+	void loadMap(const QString &filename);
+	void fillMap(const QString &filename);
+	void expandObstacles();
+
+	FloodPlanner &setStart(QPoint start);
+	FloodPlanner &setEnd(QPoint end);
+
+	bool isTileValid(const Map &map, QPoint point);
+	void markTiles(Map &map, QPoint curr, QPoint end, int distance = 0);
+
+private:
+	std::shared_ptr<Map> m_map;
+	QPoint m_start;
+	QPoint m_end;
 };
+
+std::ostream &operator<<(std::ostream &os, const FloodPlanner::Map &map);
 
